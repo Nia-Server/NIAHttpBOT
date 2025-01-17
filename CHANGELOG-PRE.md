e3fc0466ae499f2104e0ea4a7782ab404ccc2c35
NIANIANKNIA <nianianknia@163.com>
2025-01-17 15:05:29 +0800

    修复QQBOTAPI接受非200状态码报错的bug

--- CHANGELOG-PRE.md
+++ CHANGELOG-PRE.md
@@ -1,4 +1,4 @@
-# v1.0.0-pre-1 更新日志
+# v1.0.0-pre-2 更新日志
 
 [![NIAHttpBOT-VERSION](https://img.shields.io/badge/NIAHttpBOT-v1.0.0-orange?style=for-the-badge&logo=appveyor)](https://github.com/Nia-Server/NIAHttpBOT/) [![NapCatQQ-VERSION](https://img.shields.io/badge/NapCatQQ-v4.3.3-green?style=for-the-badge&logo=appveyor)](https://github.com/NapNeko/NapCatQQ/releases/tag/v4.3.3)
 
@@ -6,21 +6,17 @@
 
 > **预发布版本提醒**：这是一个**预览版本**，可能存在一些bug，仅供测试，请勿在正式生产环境使用本版本！
 
-> **未完成开发版本提醒**：该版本**部分功能仍然在开发状态中**，可能出现包括但不限于服务器无法正常运行等问题，仅供开发者预览，请勿使用！
-
-## 优化
-
-**自项目从主项目分离后，我们将从原来基于LLONEBOT的开发改为基于NapCatQQ的开发**
+> **未完成开发版本提醒**：该版本**部分功能仍然在开发状态中**，可能出现包括但不限于**服务器存档损坏**等问题，仅供开发者预览，请勿使用！
 
 ## 新增
 
-增加读取&&解析OBJ文件功能
+1. **startserver** 指令，用于启动BDS服务器
+2. **mc** 指令，用于执行Minecraft指令(当前版本仅windows支持)
+3. **stopserver** 指令，用于停止BDS服务器(当前版本仅windows支持)
+4. QQBOT新增**开服、关服、cmd**指令，用于控制BDS服务器(当前版本仅windows支持)
 
 ## 修复
 
-1. QQBOT无法正常启动的BUG
-
-2. 读取违禁词列表时候读取出错的BUG
-
+1. 调用QQBOT_API时，部分情况传回400错误而导致NIAHttpBOT崩溃的问题
 
 **配置说明：您可以前往[NIA服务器官方文档站](https://docs.mcnia.com/dev/Http-Bot.html)查看具体部署过程！**
--- src/QQBot_API.cpp
+++ src/QQBot_API.cpp
@@ -7,18 +7,21 @@ QQBot::QQBot(const std::string& IPAddress, int ClientPort)
 int32_t QQBot::send_private_message(const std::string & user_id, const std::string & message, bool auto_escape) {
     std::string auto_escape_str = auto_escape ? "true" : "false";
     auto res = cli.Post("/send_private_msg", "{\"user_id\":\"" + user_id + "\",\"message\":\"" + message + "\",\"auto_escape\":\"" + auto_escape_str +"\"}", "application/json");
-    if (res) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok" && doc.HasMember("data")) {
             return doc["data"]["message_id"].GetInt();
         } else {
-            WARN(XX("调用API <send_private_msg()> 后，返回的数据无法解析：") + res->body);
+            WARN("调用API <send_private_msg()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
-    } else {
-        WARN(XX("调用API <send_private_msg()> 超时，请检查连接！"));
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <send_private_msg()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body);
         return -2;
+    } else {
+        WARN("调用API <send_private_msg()> 超时，请检查连接！");
+        return -3;
     }
 }
 
@@ -26,7 +29,7 @@ int32_t QQBot::send_private_message(const std::string & user_id, const std::stri
 int32_t QQBot::send_group_message(const std::string & group_id, const std::string & message, bool auto_escape) {
     std::string auto_escape_str = auto_escape ? "true" : "false";
     auto res = cli.Post("/send_group_msg", "{\"group_id\":\"" + group_id + "\",\"message\":\"" + message + "\",\"auto_escape\":\"" + auto_escape_str +"\"}", "application/json");
-    if (res.value().status == 200) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok" && doc.HasMember("data")) {
@@ -35,175 +38,197 @@ int32_t QQBot::send_group_message(const std::string & group_id, const std::strin
             WARN("调用API <send_group_msg()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
-    } else if(res.value().status != 200) {
-        rapidjson::Document doc;
-        doc.Parse(res->body.c_str());
-        WARN("调用API <send_group_msg()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据："+ res->body.c_str());
+    } else if(res && res.value().status != 200) {
+        WARN("调用API <send_group_msg()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据："+ res->body);
         return -2;
-    }  else {
+    } else {
         WARN("调用API <send_group_msg()> 超时，请检查连接！");
         return -3;
     }
 
 }
 
-int QQBot::delete_msg(const int32_t &message_id)
-{
+int QQBot::delete_msg(const int32_t &message_id) {
     auto res = cli.Post("/delete_msg", "{\"message_id\":" + std::to_string(message_id) + "}", "application/json");
-    if (res) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok") {
             return 1;
         } else {
-            WARN(XX("调用API <delete_msg()> 后，返回的数据无法解析：") + res->body);
+            WARN("调用API <delete_msg()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
-    } else {
-        WARN(XX("调用API <delete_msg()> 超时，请检查连接！"));
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <delete_msg()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body);
         return -2;
+    } else {
+        WARN("调用API <delete_msg()> 超时，请检查连接！");
+        return -3;
     }
 }
 
 int QQBot::send_like(const std::string & user_id, int times) {
     auto res = cli.Post("/send_like", "{\"user_id\":\"" + user_id + "\",\"times\":" + std::to_string(times) + "}", "application/json");
-    if (res) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok") {
             return 1;
         } else if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "failed") {
-            WARN(XX("调用API <send_like()> 后，点赞失败：") + res->body);
+            WARN("调用API <send_like()> 后，点赞失败：" + res->body);
             return 0;
         } else {
-            WARN(XX("调用API <send_like()> 后，返回的数据无法解析：") + res->body);
+            WARN("调用API <send_like()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
-    } else {
-        WARN(XX("调用API <send_like()> 超时，请检查连接！"));
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <send_like()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body);
         return -2;
+    } else {
+        WARN("调用API <send_like()> 超时，请检查连接！");
+        return -3;
     }
 }
 
 int QQBot::set_group_kick(const std::string & group_id, const std::string & user_id, bool reject_add_request) {
     std::string reject_add_request_str = reject_add_request ? "true" : "false";
     auto res = cli.Post("/set_group_kick", "{\"group_id\":\"" + group_id + "\",\"user_id\":\"" + user_id + "\",\"reject_add_request\":\"" + reject_add_request_str +"\"}", "application/json");
-    if (res) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok") {
             return 1;
         } else if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "failed") {
-            WARN(XX("调用API <set_group_kick()> 后，踢出失败：") + res->body);
+            WARN("调用API <set_group_kick()> 后，踢出失败：" + res->body);
             return 0;
         } else {
-            WARN(XX("调用API <set_group_kick()> 后，返回的数据无法解析：") + res->body);
+            WARN("调用API <set_group_kick()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
-    } else {
-        WARN(XX("调用API <set_group_kick()> 超时，请检查连接！"));
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <set_group_kick()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body);
         return -2;
+    } else {
+        WARN("调用API <set_group_kick()> 超时，请检查连接！");
+        return -3;
     }
 }
 
 int QQBot::set_group_ban(const std::string & group_id, const std::string & user_id, long long duration) {
     auto res = cli.Post("/set_group_ban", "{\"group_id\":\"" + group_id + "\",\"user_id\":\"" + user_id + "\",\"duration\":" + std::to_string(duration) + "}", "application/json");
-    if (res) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok") {
             return 1;
         } else if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "failed") {
-            WARN(XX("调用API <set_group_ban()> 后，禁言失败：") + res->body);
+            WARN("调用API <set_group_ban()> 后，禁言失败：" + res->body);
             return 0;
         } else {
-            WARN(XX("调用API <set_group_ban()> 后，返回的数据无法解析：") + res->body);
+            WARN("调用API <set_group_ban()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
-    } else {
-        WARN(XX("调用API <set_group_ban()> 超时，请检查连接！"));
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <set_group_ban()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body);
         return -2;
+    } else {
+        WARN("调用API <set_group_ban()> 超时，请检查连接！");
+        return -3;
     }
 }
 
 int QQBot::set_group_whole_ban(const std::string & group_id, bool enable) {
     std::string enable_str = enable ? "true" : "false";
     auto res = cli.Post("/set_group_whole_ban", "{\"group_id\":\"" + group_id + "\",\"enable\":\"" + enable_str + "\"}", "application/json");
-    if (res) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok") {
             return 1;
         } else if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "failed") {
-            WARN(XX("调用API <set_group_whole_ban()> 后，全群禁言失败：") + res->body);
+            WARN("调用API <set_group_whole_ban()> 后，全群禁言失败：" + res->body);
             return 0;
         } else {
-            WARN(XX("调用API <set_group_whole_ban()> 后，返回的数据无法解析：") + res->body);
+            WARN("调用API <set_group_whole_ban()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
-    } else {
-        WARN(XX("调用API <set_group_whole_ban()> 超时，请检查连接！"));
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <set_group_whole_ban()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body);
         return -2;
+    } else {
+        WARN("调用API <set_group_whole_ban()> 超时，请检查连接！");
+        return -3;
     }
 }
 
 int QQBot::set_group_admin(const std::string & group_id, const std::string & user_id, bool enable) {
     std::string enable_str = enable ? "true" : "false";
     auto res = cli.Post("/set_group_admin", "{\"group_id\":\"" + group_id + "\",\"user_id\":\"" + user_id + "\",\"enable\":\"" + enable_str + "\"}", "application/json");
-    if (res) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok") {
             return 1;
         } else if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "failed") {
-            WARN(XX("调用API <set_group_admin()> 后，设置管理员失败：") + res->body);
+            WARN("调用API <set_group_admin()> 后，设置管理员失败：" + res->body);
             return 0;
         } else {
-            WARN(XX("调用API <set_group_admin()> 后，返回的数据无法解析：") + res->body);
+            WARN("调用API <set_group_admin()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
-    } else {
-        WARN(XX("调用API <set_group_admin()> 超时，请检查连接！"));
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <set_group_admin()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body);
         return -2;
+    } else {
+        WARN("调用API <set_group_admin()> 超时，请检查连接！");
+        return -3;
     }
 }
 
 int QQBot::set_group_card(const std::string & group_id, const std::string & user_id, const std::string & card) {
     auto res = cli.Post("/set_group_card", "{\"group_id\":\"" + group_id + "\",\"user_id\":\"" + user_id + "\",\"card\":\"" + card + "\"}", "application/json");
-    if (res) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok") {
             return 1;
         } else if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "failed") {
-            WARN(XX("调用API <set_group_card()> 后，设置名片失败：") + res->body);
+            WARN("调用API <set_group_card()> 后，设置名片失败：" + res->body);
             return 0;
         } else {
-            WARN(XX("调用API <set_group_card()> 后，返回的数据无法解析：") + res->body);
+            WARN("调用API <set_group_card()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
-    } else {
-        WARN(XX("调用API <set_group_card()> 超时，请检查连接！"));
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <set_group_card()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body);
         return -2;
+    } else {
+        WARN("调用API <set_group_card()> 超时，请检查连接！");
+        return -3;
     }
 }
 
 int QQBot::set_group_name(const std::string & group_id, const std::string & group_name) {
     auto res = cli.Post("/set_group_name", "{\"group_id\":\"" + group_id + "\",\"group_name\":\"" + group_name + "\"}", "application/json");
-    if (res) {
+    if (res && res.value().status != 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok") {
             return 1;
         } else if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "failed") {
-            WARN(XX("调用API <set_group_name()> 后，设置群名称失败：") + res->body);
+            WARN("调用API <set_group_name()> 后，设置群名称失败：" + res->body);
             return 0;
         } else {
-            WARN(XX("调用API <set_group_name()> 后，返回的数据无法解析：") + res->body);
+            WARN("调用API <set_group_name()> 后，返回的数据无法解析：" + res->body);
             return -1;
         }
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <set_group_name()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body.c_str());
+        return -2;
+
     } else {
-        WARN(XX("调用API <set_group_name()> 超时，请检查连接！"));
+        WARN("调用API <set_group_name()> 超时，请检查连接！");
         return -2;
     }
 }
@@ -252,7 +277,7 @@ int QQBot::set_group_request(const std::string & flag, const std::string & sub_t
 QQBot::login_info QQBot::get_login_info() {
     login_info info;
     auto res = cli.Get("/get_login_info");
-    if (res && res->status == 200) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok" && doc.HasMember("data")) {
@@ -264,9 +289,14 @@ QQBot::login_info QQBot::get_login_info() {
             info.status = -1;
         }
         return info;
+    } else if(res && res.value().status != 200) {
+        WARN("调用API <get_login_info()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body.c_str());
+        info.status = -2;
+        return info;
+
     } else {
         WARN("调用API <get_login_info()> 超时，请检查连接！");
-        info.status = -2;
+        info.status = -3;
         return info;
     }
 }
@@ -275,7 +305,7 @@ QQBot::login_info QQBot::get_login_info() {
 QQBot::bot_status QQBot::get_status() {
     bot_status status;
     auto res = cli.Get("/get_status");
-    if (res && res->status == 200) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok" && doc.HasMember("data")) {
@@ -290,9 +320,13 @@ QQBot::bot_status QQBot::get_status() {
             status.status = -1;
         }
         return status;
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <get_status()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body.c_str());
+        status.status = -2;
+        return status;
     } else {
         WARN("调用API <get_status()> 超时，请检查连接！");
-        status.status = -2;
+        status.status = -3;
         return status;
     }
 }
@@ -300,7 +334,7 @@ QQBot::bot_status QQBot::get_status() {
 std::vector<QQBot::group_list> QQBot::get_group_list() {
     std::vector<group_list> group_list;
     auto res = cli.Get("/get_group_list");
-    if (res && res->status == 200) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok" && doc.HasMember("data")) {
@@ -318,8 +352,12 @@ std::vector<QQBot::group_list> QQBot::get_group_list() {
             WARN("调用API <get_group_list()> 后，无法解析返回的数据：" + res->body);
         }
         return group_list;
-    } else {
+    } else if (res && res.value().status != 200) {
         group_list.push_back({-2, 0, "", 0, 0});
+        WARN("调用API <get_group_list()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body.c_str());
+        return group_list;
+    } else {
+        group_list.push_back({-3, 0, "", 0, 0});
         WARN("调用API <get_group_list()> 超时，请检查连接！");
         return group_list;
     }
@@ -328,7 +366,7 @@ std::vector<QQBot::group_list> QQBot::get_group_list() {
 QQBot::group_member_info QQBot::get_group_member_info(const std::string & group_id, const std::string & user_id, bool no_cache) {
     group_member_info info;
     auto res = cli.Post("/get_group_member_info", "{\"group_id\":\"" + group_id + "\",\"user_id\":\"" + user_id + "\",\"no_cache\":" + (no_cache ? "true" : "false") + "}", "application/json");
-    if (res && res->status == 200) {
+    if (res && res.value().status == 200) {
         rapidjson::Document doc;
         doc.Parse(res->body.c_str());
         if (doc.HasMember("status") && std::string(doc["status"].GetString()) == "ok" && doc.HasMember("data")) {
@@ -352,9 +390,13 @@ QQBot::group_member_info QQBot::get_group_member_info(const std::string & group_
             info.status = -1;
         }
         return info;
+    } else if (res && res.value().status != 200) {
+        WARN("调用API <get_group_member_info()> 后，状态码异常[" + std::to_string(res.value().status) + "]收到的数据：" + res->body.c_str());
+        info.status = -2;
+        return info;
     } else {
         WARN("调用API <get_group_member_info()> 超时，请检查连接！");
-        info.status = -2;
+        info.status = -3;
         return info;
     }
 }
